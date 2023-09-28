92c414a57279c105395fce5e880072fc555d50be
priyanshthakore <45281958+priyanshthakore@users.noreply.github.com>
2023-09-28 23:00:03 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello world")
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
